6637c7d63d76a088c7f188c3930619591ef530d3
RaphaZY <raphacoelho3790@gmail.com>
2024-05-02 22:13:44 -0300

    ojo

---
+++ cs/js07/js/scripts.js
@@ -0,0 +1,32 @@
+var MIN_COMPRIMENTO = 50;
+var MAX_COMPRIMENTO = 600;
+var INCREMENTO = 100;
+var TEMPO = 50;
+
+var timeout;
+
+function amplia(){
+    var div = document.getElementById('barra');
+    if (parseInt(div.style.width) < MAX_COMPRIMENTO){
+        div.style.width = (parseInt(div.style.width) + INCREMENTO) + 'px';
+        if(timeout)
+            clearTimeout(timeout);
+        timeout = setTimeout(amplia,TEMPO);
+    }
+}
+
+function reduz(){
+    var div = document.getElementById('barra');
+    if (parseInt(div.style.width) > MIN_COMPRIMENTO){
+        div.style.width = (parseInt(div.style.width) - INCREMENTO) + 'px';
+        if(timeout)
+            clearTimeout(timeout);
+        timeout = setTimeout(reduz,TEMPO);
+    }
+}
+
+function inicializa(){
+    var div = document.getElementById('barra');
+    div.onmouseover = amplia;
+    div.onmouseout = reduz;
+}
